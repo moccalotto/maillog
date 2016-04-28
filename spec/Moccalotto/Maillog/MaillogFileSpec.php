745dde5a724301a34d34e8e8831e3430e5dc40d7
Kim Ravn Hansen <moccalotto@gmail.com>
2016-04-28 18:52:03 +0800

    FIX: make tests work
    Tests did not pass because they did not account for yearly loop-around.
    
    Log files do not contain year info - so we need to simulate that we're
    always operating within the current year.

--- spec/Moccalotto/Maillog/MaillogFileSpec.php
+++ spec/Moccalotto/Maillog/MaillogFileSpec.php
@@ -8,17 +8,30 @@ use DateTime;
 
 class MaillogFileSpec extends ObjectBehavior
 {
+    /**
+     * @var string
+     */
+    protected $file = __DIR__ . '/maillog';
+
     public function it_is_initializable()
     {
-        $this->beConstructedWith(__DIR__.'/maillog', new LineParser());
+        $this->beConstructedWith($this->file, new LineParser());
+
         $this->shouldHaveType('Moccalotto\Maillog\MaillogFile');
     }
 
     public function it_can_parse_a_file()
     {
-        $this->beConstructedWith(__DIR__.'/maillog', new LineParser());
+        // HACK: we need to ensure that we know the mtime of the file.
+        // Otherwise the tests will only work AFTER nov 29th of the given year.
+        touch($this->file, strtotime('Nov 29 13:33:00 UTC'));
+
+        $this->beConstructedWith($this->file, new LineParser());
+
         $this->getLinesNewerThan(new DateTime('1999-01-01 00:00:00 UTC'))->shouldHaveCount(3);
+
         $this->getLinesNewerThan(new DateTime('Nov 29 13:30:21 UTC'))->shouldHaveCount(1);
+
         $this->getLinesNewerThan(new DateTime('2200-01-01 UTC'))->shouldHaveCount(0);
     }
 }
